4971d44bcda7c842ee4546675774933419c0920a
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-08-25 11:00:13 -0300

    The code (v1.0.0)

--- src/Contador.java
+++ src/Contador.java
@@ -1,26 +1,71 @@
 import java.util.Scanner;
+import exceptions.ParametrosInvalidosException;
 
+/**
+ * Classe que faz uma contagem simples para um desafio da DIO
+ */
 public class Contador {
+    /**
+     * Método main a ser chamado quando a classe é invocada
+     * via terminal
+     *
+     * @param args Parâmetros passados via CLI
+     */
     public static void main(String[] args) {
-        Scanner terminal = new Scanner(System.in);
-        System.out.println("Digite o primeiro parâmetro");
-        int parametroUm = terminal.??;
-        System.out.println("Digite o segundo parâmetro");
-        int parametroDois = terminal.??;
+        int parametroUm = receberParametro("Digite o primeiro parâmetro");
+        int parametroDois = receberParametro("Digite o segundo parâmetro");
         
         try {
-            //chamando o método contendo a lógica de contagem
             contar(parametroUm, parametroDois);
         
-        }catch (? exception) {
-            //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+        }catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
         }
-        
     }
+
+    /**
+     * Permite informar uma mensagem para instruir o usuário a passar um
+     * parâmetro via CLI. Recee este parâmetro também
+     *
+     * @param mensagem Mensagem a ser passada para o usuário antes de receber
+     *                 o parâmetro
+     *
+     * @return Retorna o parâmetro passado pelo usuário,
+     *         parâmetrorecebido via CLI
+     */
+    static int receberParametro(String mensagem) {
+        Scanner terminal = new Scanner(System.in);
+        int parametro;
+        while (true) {
+            try {
+                System.out.print(mensagem + ": ");
+                parametro = terminal.nextInt();
+                break;
+            } catch (Exception e) {
+                System.out.println("\nVocê precisa passar um número inteiro.\n");
+                terminal.next();
+            }
+        }
+        return parametro;
+    }
+
+    /**
+     * Faz uma contagem entre o parâmetro um e o parâmetro dois
+     *
+     * @param parametroUm   Parâmetro 1 que o usuário informou anteriormente
+     * @param parametroDois Parâmetro 2 que o usuário informou anteriormente
+     * @throws ParametrosInvalidosException Se parametroUm > parametroDois
+     */
     static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
         //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-        
-        int contagem = parametroDois - parametroUm;
-        //realizar o for para imprimir os números com base na variável contagem
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        } else {
+            System.out.println("Vamos contar do número um ao número dois (incluindo este)");
+
+            for (int i = parametroUm; i <= parametroDois; i++) {
+                System.out.println("O número desta iteração é o: " + i);
+            }
+        }
     }
 }
